8d0634e2dab8502be1e53f4e0cb066ddf4c51aba
selvaganapathi <selvaganapathim@ami.com>
2022-11-01 00:06:42 +0530

    Add watchdog2 type discrete sensor
    Description
    -Add service for system management module related sensor
    -This service will handle watchdog2, sel type discrte sensor
    -Currently class added to handle watchdog2 discrete sensor
    -watchdogsensor class monitors signal from phosphor-watchdog service and
    state asserted accordingly
    
    Tested
    -Able to build successfully and sensor listing
    -Set watchdog timer action to "HardReset"
    ipmitool raw 0x06 0x24 0x01 0x01 0x00 0x00 0x10 0x00
    -Reset watchdog
    ipmitool raw 0x06 0x22
    -Watchdog2 Sensor state asserted as "HardReset"
    
    D-bus service and object:
    root@dgx-a100-dp:~# busctl tree xyz.openbmc_project.SystemSensor
    `-/xyz
      `-/xyz/openbmc_project
        `-/xyz/openbmc_project/sensors
          `-/xyz/openbmc_project/sensors/motherboard
            `-/xyz/openbmc_project/sensors/motherboard/watchdog
              `-/xyz/openbmc_project/sensors/motherboard/watchdog/watchdog2
    
    NAME                                        TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable         interface -         -                                        -
    .Introspect                                 method    -         s                                        -
    org.freedesktop.DBus.Peer                   interface -         -                                        -
    .GetMachineId                               method    -         s                                        -
    .Ping                                       method    -         -                                        -
    org.freedesktop.DBus.Properties             interface -         -                                        -
    .Get                                        method    ss        v                                        -
    .GetAll                                     method    s         a{sv}                                    -
    .Set                                        method    ssv       -                                        -
    .PropertiesChanged                          signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Association.Definitions interface -         -                                        -
    .Associations                               property  a(sss)    1 "chassis" "all_sensors" "/xyz/openb... emits-change writable
    xyz.openbmc_project.Inventory.Item.Watchdog interface -         -                                        -
    .Status                                     property  s         "HardReset"                              emits-change writable
    
    SDR in Verbose:
    Sensor ID              : watchdog2 (0x22)
     Entity ID             : 46.1 (Management Controller Firmware)
     Sensor Type (Discrete): Watchdog2 (0x23)
     Sensor Reading        : 0h
     Event Message Control : Per-threshold
     States Asserted       : Watchdog2
                             [Hard reset]
     Event Status          : Event Messages Disabled
     Event Enable          : Event Messages Disabled
     Assertions Enabled    : Watchdog2
                             [Timer expired]
                             [Hard reset]
                             [Power down]
                             [Power cycle]
     Deassertions Enabled  : Watchdog2
                             [Timer expired]
                             [Hard reset]
                             [Power down]
                             [Power cycle]
     OEM                   : 0
    
    SDR List:
    SPD FAN SYS7 R   | 21h | ok  |  0.1 | 10976 RPM
    watchdog2        | 22h | ok  | 46.1 | Hard reset
    PWR PSU0         | 23h | ok  |  0.1 | 351 Watts
    
    Sensor List:
    SPD FAN SYS7 R   | 7154.000   | RPM        | ok    | na        | 2156.000  | na        | na        | na        | na
    watchdog2        | 0x0        | discrete   | 0x0200| na        | na        | na        | na        | na        | na
    PWR PSU0         | 195.000    | Watts      | ok    | na        | 0.000     | na        | 3315.000  | 3315.000  | na
    
    Fixes JIRA https://jirasw.nvidia.com/browse/DGXOPENBMC-4609
    
    Signed-off-by: selvaganapathi <selvaganapathim@ami.com>

---
+++ include/WatchdogSensor.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Utils.hpp>
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/posix/stream_descriptor.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <xyz/openbmc_project/Association/Definitions/server.hpp>
+
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+using AssocInterface = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Association::server::Definitions>;
+
+class WatchdogSensor :
+    public AssocInterface,
+    public std::enable_shared_from_this<WatchdogSensor>
+{
+  public:
+    WatchdogSensor(
+        sdbusplus::asio::object_server& objectServer,
+        std::shared_ptr<sdbusplus::asio::connection>& conn,
+        /*boost::asio::io_service& io,*/ const std::string& sensorName,
+        const std::string& sensorConfiguration);
+    ~WatchdogSensor() override;
+
+    std::string name;
+    std::string status;
+
+  private:
+    std::shared_ptr<sdbusplus::asio::dbus_interface> sensorInterface;
+    sdbusplus::asio::object_server& objServer;
+    std::shared_ptr<sdbusplus::bus::match::match> watchdogEventMatcher;
+};
---
+++ src/WatchdogSensor.cpp
@@ -0,0 +1,121 @@
+#include <unistd.h>
+
+#include <WatchdogSensor.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <optional>
+#include <string>
+#include <vector>
+
+WatchdogSensor::WatchdogSensor(
+    sdbusplus::asio::object_server& objectServer,
+    std::shared_ptr<sdbusplus::asio::connection>& conn,
+    /*boost::asio::io_service& io,*/
+    const std::string& sensorName, const std::string& sensorConfiguration) :
+    AssocInterface(static_cast<sdbusplus::bus::bus&>(*conn),
+                   ("/xyz/openbmc_project/sensors/motherboard/watchdog/" +
+                    escapeName(sensorName))
+                       .c_str(),
+                   AssocInterface::action::defer_emit),
+    std::enable_shared_from_this<WatchdogSensor>(), name(sensorName),
+    objServer(objectServer)
+{
+    sensorInterface = objectServer.add_interface(
+        ("/xyz/openbmc_project/sensors/motherboard/watchdog/" +
+         escapeName(sensorName))
+            .c_str(),
+        "xyz.openbmc_project.Inventory.Item.Watchdog");
+    sensorInterface->register_property(
+        "Status", status,
+        [&](const std::string& newStatus, std::string& oldStatus) {
+            oldStatus = newStatus;
+            status = newStatus;
+            return 1;
+        });
+
+    fs::path p(sensorConfiguration);
+    AssociationList assocs = {};
+    assocs.emplace_back(
+        std::make_tuple("chassis", "all_sensors", p.parent_path().string()));
+    sdbusplus::xyz::openbmc_project::Association::server::Definitions::
+        associations(assocs);
+    if (!sensorInterface->initialize())
+    {
+        std::cerr << "error initializing value interface\n";
+    }
+
+    auto watchdogEventMatcherCallback = [this, conn](
+                                            sdbusplus::message::message& msg) {
+        std::optional<std::string_view> expireAction;
+
+        sdbusplus::message::message getWatchdogStatus =
+            conn->new_method_call(msg.get_sender(), msg.get_path(),
+                                  "org.freedesktop.DBus.Properties", "GetAll");
+        getWatchdogStatus.append("xyz.openbmc_project.State.Watchdog");
+        boost::container::flat_map<std::string,
+                                   std::variant<std::string, uint64_t, bool>>
+            watchdogStatus;
+
+        try
+        {
+            sdbusplus::message::message getWatchdogStatusResp =
+                conn->call(getWatchdogStatus);
+            getWatchdogStatusResp.read(watchdogStatus);
+        }
+        catch (const sdbusplus::exception_t&)
+        {
+            std::cerr << "error getting watchdog status from " << msg.get_path()
+                      << "\n";
+            return;
+        }
+
+        auto getExpireAction = watchdogStatus.find("ExpireAction");
+        if (getExpireAction != watchdogStatus.end())
+        {
+            expireAction = std::get<std::string>(getExpireAction->second);
+            expireAction->remove_prefix(std::min(
+                expireAction->find_last_of(".") + 1, expireAction->size()));
+        }
+
+        if (*expireAction == "HardReset")
+        {
+            sensorInterface->set_property(
+                "Status", static_cast<std::string>("HardReset"));
+        }
+        else if (*expireAction == "PowerOff")
+        {
+            sensorInterface->set_property("Status",
+                                          static_cast<std::string>("PowerOff"));
+        }
+        else if (*expireAction == "PowerCycle")
+        {
+            sensorInterface->set_property(
+                "Status", static_cast<std::string>("PowerCycle"));
+        }
+        else if (*expireAction == "None")
+        {
+            sensorInterface->set_property(
+                "Status", static_cast<std::string>("TimerExpired"));
+        }
+        else if (*expireAction == "TimerInterrupt")
+        {
+            sensorInterface->set_property(
+                "Status", static_cast<std::string>("TimerInterrupt"));
+        }
+    };
+
+    watchdogEventMatcher = std::make_shared<sdbusplus::bus::match::match>(
+        static_cast<sdbusplus::bus::bus&>(*conn),
+        "type='signal',interface='xyz.openbmc_project.Watchdog',"
+        "member='Timeout'",
+        std::move(watchdogEventMatcherCallback));
+}
+
+WatchdogSensor::~WatchdogSensor()
+{
+    objServer.remove_interface(sensorInterface);
+}
