a88bb5221395b7830703c326b60e30a4a92e43df
selvaganapathi <selvaganapathim@ami.com>
2023-01-09 14:44:35 +0530

    Add PSU Redundancy status discrete sensor
    Description
    - implements new service called "xyz.openbmc_project.psuredundancy"
    - Checking PSU status and redundancy state determined with redundant
    PSU count and updated
    
    Tested
    - Successfully build and PSU redundancy sensor objects
    listed
    
     busctl introspect xyz.openbmc_project.psuredundancy /xyz/openbmc_project/sensors/power/PSU/PSU_REDUNDANCY
    NAME                                              TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable               interface -         -                                        -
    .Introspect                                       method    -         s                                        -
    org.freedesktop.DBus.Peer                         interface -         -                                        -
    .GetMachineId                                     method    -         s                                        -
    .Ping                                             method    -         -                                        -
    org.freedesktop.DBus.Properties                   interface -         -                                        -
    .Get                                              method    ss        v                                        -
    .GetAll                                           method    s         a{sv}                                    -
    .Set                                              method    ssv       -                                        -
    .PropertiesChanged                                signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Association.Definitions       interface -         -                                        -
    .Associations                                     property  a(sss)    1 "chassis" "all_sensors" "/xyz/openb... emits-change wre
    xyz.openbmc_project.Control.PowerSupplyRedundancy interface -         -                                        -
    .Status                                           property  s         "fullyRedundant"                         emits-change wre
    
    busctl tree xyz.openbmc_project.psuredundancy
    `-/xyz
      `-/xyz/openbmc_project
        `-/xyz/openbmc_project/sensors
          `-/xyz/openbmc_project/sensors/power
            `-/xyz/openbmc_project/sensors/power/PSU
              `-/xyz/openbmc_project/sensors/power/PSU/PSU_REDUNDANCY
    
    Fixes JIRA https://jirasw.nvidia.com/browse/DGXOPENBMC-4609
    
    Signed-off-by: selvaganapathi <selvaganapathim@ami.com>

---
+++ include/PSURedundancy.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <Utils.hpp>
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/posix/stream_descriptor.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <xyz/openbmc_project/Association/Definitions/server.hpp>
+
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+constexpr auto OPERATIONAL_STATE_IFACE =
+    "xyz.openbmc_project.State.Decorator.OperationalStatus";
+constexpr auto PSU_SERVICE = "com.Nvidia.Powersupply";
+constexpr auto PSU_OBJ =
+    "/xyz/openbmc_project/inventory/system/chassis/motherboard/powersupply";
+constexpr auto PSU_BASE_OBJ =
+    "/xyz/openbmc_project/inventory/system/chassis/motherboard";
+
+namespace fs = std::filesystem;
+
+using AssocInterface = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Association::server::Definitions>;
+
+class PSURedundancy :
+    public AssocInterface,
+    public std::enable_shared_from_this<PSURedundancy>
+{
+  public:
+    PSURedundancy(sdbusplus::asio::object_server& objectServer,
+                  std::shared_ptr<sdbusplus::asio::connection>& conn,
+                  const std::string& sensorName, int totalPSUCount,
+                  int redundantPSUCount, int sufficientPSUCount,
+                  const std::string& sensorConfiguration);
+    ~PSURedundancy() override;
+
+    std::string name;
+    std::string status;
+    bool redundancyLost = false;
+    int totalPSU;
+    int redundantPSU;
+    int workablePSU;
+    int previousWorkablePSU;
+    int sufficientPSU;
+
+  private:
+    std::shared_ptr<sdbusplus::asio::dbus_interface> sensorInterface;
+    sdbusplus::asio::object_server& objServer;
+    std::shared_ptr<sdbusplus::bus::match::match> psuEventMatcher;
+    void setStatus();
+};
---
+++ src/PSURedundancy.cpp
@@ -0,0 +1,213 @@
+#include <unistd.h>
+
+#include <PSURedundancy.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <optional>
+#include <string>
+#include <vector>
+
+PSURedundancy::PSURedundancy(sdbusplus::asio::object_server& objectServer,
+                             std::shared_ptr<sdbusplus::asio::connection>& conn,
+                             const std::string& sensorName, int totalPSUCount,
+                             int redundantPSUCount, int sufficientPSUCount,
+                             const std::string& sensorConfiguration) :
+    AssocInterface(
+        static_cast<sdbusplus::bus::bus&>(*conn),
+        ("/xyz/openbmc_project/sensors/power/PSU/" + escapeName(sensorName))
+            .c_str(),
+        AssocInterface::action::defer_emit),
+    std::enable_shared_from_this<PSURedundancy>(), name(sensorName),
+    totalPSU(totalPSUCount), redundantPSU(redundantPSUCount),
+    previousWorkablePSU(totalPSUCount), sufficientPSU(sufficientPSUCount),
+    objServer(objectServer)
+{
+    sensorInterface = objectServer.add_interface(
+        ("/xyz/openbmc_project/sensors/power/PSU/" + escapeName(sensorName))
+            .c_str(),
+        "xyz.openbmc_project.Control.PowerSupplyRedundancy");
+    sensorInterface->register_property(
+        "Status", status,
+        [&](const std::string& newStatus, std::string& oldStatus) {
+            oldStatus = newStatus;
+            status = newStatus;
+            return 1;
+        });
+
+    sensorInterface->register_property(
+        "RedundancyLost", redundancyLost,
+        [&](const bool& newStatus, bool& oldStatus) {
+            oldStatus = newStatus;
+            status = newStatus;
+            return 1;
+        });
+
+    fs::path p(sensorConfiguration);
+    AssociationList assocs = {};
+    assocs.emplace_back(
+        std::make_tuple("chassis", "all_sensors", p.parent_path().string()));
+    sdbusplus::xyz::openbmc_project::Association::server::Definitions::
+        associations(assocs);
+    if (!sensorInterface->initialize())
+    {
+        std::cerr << "error initializing sensor interface\n";
+    }
+
+    /* Get initial status by reading totalPSU status */
+    workablePSU = 0;
+    previousWorkablePSU = totalPSU;
+    for (int i = 0; i < totalPSU; i++)
+    {
+        std::variant<bool> value;
+        std::string objPath = PSU_OBJ + std::to_string(i);
+        auto method = static_cast<sdbusplus::bus::bus&>(*conn).new_method_call(
+            PSU_SERVICE, objPath.c_str(), "org.freedesktop.DBus.Properties",
+            "Get");
+
+        method.append(OPERATIONAL_STATE_IFACE, "Functional");
+
+        auto reply = static_cast<sdbusplus::bus::bus&>(*conn).call(method);
+
+        if (reply.is_method_error())
+        {
+            std::cerr << "error\n";
+        }
+
+        reply.read(value);
+        if (std::get<bool>(value))
+        {
+            workablePSU++;
+        }
+    }
+    setStatus();
+
+    auto psuEventMatcherCallback = [this,
+                                    conn](sdbusplus::message::message& msg) {
+        std::string objectName;
+        boost::container::flat_map<std::string, std::variant<bool>> values;
+        try
+        {
+            msg.read(objectName, values);
+        }
+        catch (const sdbusplus::exception_t& e)
+        {
+            std::cerr << "Failed to read message from PSU Event\n";
+            return;
+        }
+
+        std::string psuEventName = "Functional";
+        auto findEvent = values.find(psuEventName);
+        if (findEvent != values.end())
+        {
+            bool* functional = std::get_if<bool>(&(findEvent->second));
+            if (functional == nullptr)
+            {
+                std::cerr << "Unable to get valid functional status\n";
+                return;
+            }
+            if (*functional)
+            {
+                workablePSU++;
+            }
+            else
+            {
+                workablePSU--;
+            }
+        }
+
+        setStatus();
+    };
+
+    /* create properties changed signal handler for the status change*/
+    psuEventMatcher = std::make_shared<sdbusplus::bus::match::match>(
+        static_cast<sdbusplus::bus::bus&>(*conn),
+        "type='signal',member='PropertiesChanged',path_namespace='" +
+            std::string(PSU_BASE_OBJ) + "',arg0namespace='" +
+            std::string(OPERATIONAL_STATE_IFACE) + "'",
+        std::move(psuEventMatcherCallback));
+}
+
+PSURedundancy::~PSURedundancy()
+{
+    objServer.remove_interface(sensorInterface);
+}
+
+void PSURedundancy::setStatus()
+{
+
+    if (workablePSU > previousWorkablePSU)
+    {
+        if (workablePSU >= redundantPSU)
+        {
+            if (workablePSU == totalPSU)
+            {
+                // When all PSU are work correctly, it is full redundant - 00
+                sensorInterface->set_property(
+                    "Status", static_cast<std::string>("fullyRedundant"));
+            }
+            else if (previousWorkablePSU < redundantPSU)
+            {
+                // Not all PSU can work correctly but system still in
+                // redundancy mode and previous status is non redundant - 07
+                sensorInterface->set_property(
+                    "Status", static_cast<std::string>("redundancyRegained"));
+            }
+        }
+        else if (previousWorkablePSU == sufficientPSU)
+        {
+            // Now system is not in redundancy mode but still some PSU are
+            // workable and previously there is no sufficient workable PSU in
+            // the system - 04
+            sensorInterface->set_property("RedundancyLost", false);
+            sensorInterface->set_property(
+                "Status", static_cast<std::string>("sufficient"));
+        }
+    }
+    else if (workablePSU < previousWorkablePSU)
+    {
+        if (workablePSU >= redundantPSU)
+        {
+            // One PSU is now not workable, but other workable PSU can still
+            // support redundancy mode - 02
+            sensorInterface->set_property(
+                "Status", static_cast<std::string>("redundancyDegraded"));
+
+            if (previousWorkablePSU == totalPSU)
+            {
+                // One PSU become not workable and system was in full
+                // redundancy mode - 06
+                sensorInterface->set_property(
+                    "Status",
+                    static_cast<std::string>("redundancyDegradedFromFull"));
+            }
+        }
+        else
+        {
+            if (previousWorkablePSU >= redundantPSU)
+            {
+                // No enough workable PSU to support redundancy and
+                // previously system is in redundancy mode - 01
+                sensorInterface->set_property("RedundancyLost", true);
+                if (workablePSU > sufficientPSU)
+                {
+                    // There still some sufficient PSU, but system is not
+                    // in redundancy mode - 03
+                    sensorInterface->set_property(
+                        "Status",
+                        static_cast<std::string>("sufficientFromRedundant"));
+                }
+            }
+            if (workablePSU == sufficientPSU)
+            {
+                // No sufficient PSU on the system - 05
+                sensorInterface->set_property(
+                    "Status", static_cast<std::string>("insufficient"));
+            }
+        }
+    }
+    previousWorkablePSU = workablePSU;
+}
