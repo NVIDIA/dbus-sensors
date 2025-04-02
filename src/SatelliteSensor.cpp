cebe11c848a3e674cd222a646b3b34415ce65ea8
Tony Ao <tao@nvidia.com>
2025-04-02 08:08:32 -0700

    Fixed invalid readings for triggering threshold events
    Situation:
    - if data is not ready yet in eeprom, satellitesensor will read all 0xff
    at this moment. 0xff will be treated as a normal reading 0, resulting in
    lower critical/warning events will be triggered.
    
    Change:
    - return an error when reading data is not ready
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5145269

--- src/SatelliteSensor.cpp
+++ src/SatelliteSensor.cpp
@@ -222,7 +222,7 @@ int i2cCmd(uint8_t bus, uint8_t addr, size_t offset, T* reading, uint8_t length)
     // there is no reading if all bytes are 0xff
     if (emptyBytes == length)
     {
-        *reading = 0;
+        return -1;
     }
     else
     {
