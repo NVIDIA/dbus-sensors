131d7969258d81897db76294732f3c522b5d62f7
Brooks Keough <bkeough@nvidia.com>
2025-05-08 14:59:59 -0700

    Fixed tach sensors making critical SELs after power cycle
    Tach sensors were sometimes getting below their critical thresholds
    after they lost power during a split second when the BMC's run power
    status hadn't been updated. This change adopts a delay, similar to what
    the ADCSensor service uses to address this issue.
    
    Signed-off-by: Brooks Keough <bkeough@nvidia.com>
    
    Fixes NVBug https://nvbugspro.nvidia.com/bug/5199748

--- src/ADCSensor.cpp
+++ src/ADCSensor.cpp
@@ -239,5 +239,6 @@ void ADCSensor::checkThresholds()
         return;
     }
 
-    thresholds::checkThresholdsPowerDelay(weak_from_this(), thresholdTimer);
+    [[maybe_unused]] bool status =
+        thresholds::checkThresholdsPowerDelay(weak_from_this(), thresholdTimer);
 }
