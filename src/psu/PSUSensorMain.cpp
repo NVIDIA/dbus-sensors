564f6252ebfde41c4f4a9b9c532c1e25370d8e82
Frans Fourie <ffourie@nvidia.com>
2026-04-16 20:58:05 -0700

    fix: whitelist mp5926 in psusensor
    Add mp5926 to the PSU sensor type map so psusensor can bind devices
    configured with Type: "mp5926".
    
    This userspace change depends on the mp5926 kernel driver backport being
    present in the NVIDIA linux develop-6.12 branch (upstream commit
    ca2cf35544926dd668b28f63a5babb8dc36a26d6).
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5745313

--- src/psu/PSUSensorMain.cpp
+++ src/psu/PSUSensorMain.cpp
@@ -120,6 +120,7 @@ static const I2CDeviceTypeMap sensorTypes{
     {"MP2973", I2CDeviceType{"mp2973", true}},
     {"MP2975", I2CDeviceType{"mp2975", true}},
     {"MP5023", I2CDeviceType{"mp5023", true}},
+    {"mp5926", I2CDeviceType{"mp5926", true}},
     {"MP5990", I2CDeviceType{"mp5990", true}},
     {"MPQ8785", I2CDeviceType{"mpq8785", true}},
     {"NCP4200", I2CDeviceType{"ncp4200", true}},
